43c466093c630f7c6c9953bb8722e8e361936fbb
Bruno Alves <vespidhook@gmail.com>
2022-11-08 19:31:05 -0300

    header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
+import { Header } from '@components/Header';
+
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>    
-      
+      <Header />      
     </Container>
   );
 }
