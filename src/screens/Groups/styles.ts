ef0e895d57eb040c2ae69efb4ffaedc155650c23
Bruno Alves <vespidhook@gmail.com>
2022-11-08 18:55:05 -0300

    fonts

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -3,11 +3,5 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
     flex: 1;
     background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-    align-items: center;
-    justify-content: center;
-`;
-
-export const Title = styled.Text`
-    color: #fff;
-    font-size: 32px;
-`;
+    padding: 24px;
+`; 
