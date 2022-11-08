57afa4e3d6ae7504c46b172a27f86b90a7c62c33
Bruno Alves <vespidhook@gmail.com>
2022-11-08 07:48:39 -0300

    theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native';
-
 import { Container, Title } from './styles';
 
 export function Groups() {
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: #000;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
     align-items: center;
     justify-content: center;
 `;
 
 export const Title = styled.Text`
     color: #fff;
-    font-size: 36px;
+    font-size: 32px;
 `;
