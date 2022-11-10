caadec783d3c9e4cef8f9700a3694dddf2c00292
Bruno Alves <vespidhook@gmail.com>
2022-11-10 20:11:53 -0300

    screen players

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,11 +1,14 @@
 import { TextInputProps } from "react-native";
+import { useTheme } from "styled-components/native"; 
 
 import { Container } from "./styles";
 
 export function Input({...rest}: TextInputProps) {
+    const { COLORS } = useTheme();
+
     return (
         <Container 
-            placeholderTextColor="gray"
+            placeholderTextColor={COLORS.GRAY_300}
             {...rest}
         />
     );
---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,21 @@
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+
+import { Container } from "./styles";
+
+export function Players() {
+    return (
+        <Container>
+            <Header showBackButton />
+
+            <Highlight
+                title="Nome da turma"
+                subtitle="Adicione a galera e separe os times"
+            />
+
+            <ButtonIcon />
+
+        </Container>
+    );
+}
