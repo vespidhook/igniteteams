f7e2fe08c6644d7e2a3507ec6dec93e1fbe15f52
Bruno Alves <vespidhook@gmail.com>
2022-11-09 23:17:04 -0300

    components

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import { TextInputProps } from "react-native";
+
+import { Container } from "./styles";
+
+export function Input({...rest}: TextInputProps) {
+    return (
+        <Container 
+            placeholderTextColor="gray"
+            {...rest}
+        />
+    );
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,45 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>    
-      <Header />      
+      <Header />
+
+      <Highlight 
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      /> 
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Que tal cadastrar a primeira turma?"
+          />
+        )}
+      />
+
+      <Button
+        title="Criar nova turma"
+      />
     </Container>
   );
 }
