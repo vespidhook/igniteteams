28780d0f8a5ee47018f35d9aeb46b0b05faa0e13
Bruno Alves <vespidhook@gmail.com>
2022-11-11 08:40:24 -0300

    form

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,15 +1,20 @@
 import { TouchableOpacityProps } from "react-native";
 
-import { Container, Icon } from "./styles";
+import { ButtonIconTypeStyleProps, Container, Icon } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
 
 type Props = TouchableOpacityProps & {
-    
+    icon: keyof typeof MaterialIcons.glyphMap;
+    type?: ButtonIconTypeStyleProps
 }
 
-export function ButtonIcon({ }: Props) {
+export function ButtonIcon({ icon, type = 'PRIMARY', ...rest }: Props) {
     return (
-        <Container>
-            <Icon name="home" type="PRIMARY" />
+        <Container {...rest}>
+            <Icon
+                name={icon}
+                type={type}
+            />
         </Container>
     );
 }
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,9 @@
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
 
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 
 export function Players() {
     return (
@@ -13,8 +14,15 @@ export function Players() {
                 title="Nome da turma"
                 subtitle="Adicione a galera e separe os times"
             />
-
-            <ButtonIcon />
+            <Form>
+                <Input 
+                    placeholder="Nome da pessoa"
+                    autoCorrect={false}
+                />
+                <ButtonIcon 
+                    icon="add" 
+                />
+            </Form>
 
         </Container>
     );
