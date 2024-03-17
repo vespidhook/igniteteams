8c672a64832604aad628ba17ac94f461f4e1ef1a
Bruno Alves <vespidhook@gmail.com>
2024-03-17 16:05:47 -0300

    header

--- App.tsx
+++ App.tsx
@@ -3,12 +3,18 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import Groups from "@screens/Groups";
 import theme from "@theme/index"; 
 import { Loading } from "@components/Loading";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
       { fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
     )
