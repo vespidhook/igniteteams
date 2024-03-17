d2f479b6d0a198cf82832b30ec1047dbebb117b8
Bruno Alves <vespidhook@gmail.com>
2024-03-17 00:18:29 -0300

    fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
+import { ThemeProvider } from "styled-components/native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import Groups from "@screens/Groups";
+import theme from "@theme/index";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  
   return (
-    <Groups />
-  )
+    <ThemeProvider theme={theme}>
+      { fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
+    )
 }
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,14 +2,6 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Title = styled.Text`
-  color: #fff;
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 16px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  padding: 24px;
 `;
