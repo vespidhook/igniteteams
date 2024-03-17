9a0551a18130c68fed6ffe29b31a1dcc6d923874
Bruno Alves <vespidhook@gmail.com>
2024-03-17 11:19:12 -0300

    Loading

--- App.tsx
+++ App.tsx
@@ -1,16 +1,15 @@
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import Groups from "@screens/Groups";
-import theme from "@theme/index";
-import { ActivityIndicator } from "react-native";
+import theme from "@theme/index"; 
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      { fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
     )
 }
