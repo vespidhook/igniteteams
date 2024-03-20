fd3ebd312790467128fc7cb2f7bc7995ba9e5683
Bruno Alves <vespidhook@gmail.com>
2024-03-20 16:48:30 -0300

    Input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from "./styles";
 import React from "react";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
 export function NewGroup(){
   return (
@@ -16,8 +17,11 @@ export function NewGroup(){
           subtitle="Converse com seus amigos"
         />
 
+        <Input />
+
         <Button 
           title="Criar grupo"
+          style={{ marginTop: 20 }}
         />
       </Content>
     </Container>
