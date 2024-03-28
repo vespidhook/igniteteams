a239547578bec0eefdde5466baec6c96e2227ec9
Bruno Alves <vespidhook@gmail.com>
2024-03-27 22:56:09 -0300

    listagem

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,10 +1,16 @@
 import { Header } from '@components/Header';
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
+import { Filter } from '@components/Filter';
+import { FlatList } from 'react-native';
+import { useState } from 'react';
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState(['Bruno', 'Joice', 'Helena']);
+
   return (
     <Container>
       <Header showBackButton />
@@ -23,8 +29,26 @@ export function Players() {
         <ButtonIcon 
           icon='add' 
         />
-      </Form>     
-      
+      </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+          <Filter
+            title={item}
+            isActive={item === team}
+            onPress={() => setTeam(item)}       
+              />
+            )}
+            horizontal
+          />
+          
+          <NumbersOfPlayers>
+            {players.length} jogadores
+          </NumbersOfPlayers>
+      </HeaderList>     
     </Container>
   );
 }
